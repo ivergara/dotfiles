09e918b177cf1cc00942b515e661dc49cdc69bb1
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2013-07-27 07:37:47 -0700

    Many fixes plug gitignore_globals.

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 dotfiles
 ========
 
-My dotfiles.
+My dotfiles for portable and upgradeable environment.
 
 ToDo
 ====
 
 - Better automation
+-- global gitignore automation
+-- Instalation/Update
 - Clean and better organize .vimrc
+
+Fixes
+=====
+
+- Terminator background error in ubuntu and increase opacity
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+mkdir $HOME/.config/terminator
 cp ./terminator/config $HOME/.config/terminator
 git clone git://github.com/robbyrussell/oh-my-zsh.git
 # Fix and automate relative paths
@@ -6,6 +7,12 @@ cp .zshrc $HOME
 # Making zsh the default shell
 chsh -s /bin/zsh
 
+mv $HOME/.vimrc ./backup/vimrc.bak
 cp .vimrc $HOME/
+mkdir $HOME/.vim/ftplugin
 cp vim/ftplugin/* $HOME/.vim/ftplugin
 vim +BuncleInstall +qall # Installing vim plugins
+
+# GIT section
+mv ./git/gitignore_globals $HOME/.gitignore_globals
+git config --global core.excludesfile ~/.gitignore_global
