70a44f96b4acfbc1eff379f2062e0b32e0578ab2
Ignacio Vergara Kausel <ivergarakausel@gmail.com>
2015-05-20 19:21:18 +0200

    Symolic linking!

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 # Terminator
 sudo apt-get install terminator
 mkdir $HOME/.config/terminator
-cp ./terminator/config $HOME/.config/terminator
+ln -s ./terminator/config $HOME/.config/terminator
 
 # Git
 sudo apt-get install git
 
 # GIT section
 cp ./git/gitignore_globals $HOME/.gitignore_globals
-git config --global core.excludesfile ~/.gitignore_global
+git config --global push.default simple
+git config --global core.excludesfile ~/.gitignore_globals
 git config --global core.editor "vim"
 git config --global user.name "Ignacio Vergara Kausel"
 git config --global user.email "ivergarakausel@gmail.com"
@@ -17,22 +18,17 @@ git config --global user.email "ivergarakausel@gmail.com"
 sudo apt-get install zsh
 
 
-# Fix and automate relative paths
-cp .zshrc $HOME
-
 # Making zsh the default shell
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-cp zsh/.zshrc  ~/.zshrc
+ln -s zsh/.zshrc  ~/.zshrc
 chsh -s /bin/zsh
 
 # Vim
 sudo apt-get install vim
 
 mv $HOME/.vimrc ./backup/vimrc.bak
-cp .vimrc $HOME/
+ln -s .vimrc $HOME/
 mkdir $HOME/.vim/ftplugin
-cp vim/ftplugin/* $HOME/.vim/ftplugin
+ln -s vim/ftplugin/* $HOME/.vim/ftplugin
 vim +BuncleInstall +qall # Installing vim plugins
-
-
